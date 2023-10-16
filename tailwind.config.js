109f6af754ce1ef144454cf8a6f6c66cbc7e83c9
Araya <arayalakew@gmail.com>
2023-10-16 12:29:15 +0200

    oct 16-2

--- tailwind.config.js
+++ tailwind.config.js
@@ -76,8 +76,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require("tailwindcss-animate"),
-    require('flowbite/plugin')
-  ],
+  plugins: [require("tailwindcss-animate")],
 };
