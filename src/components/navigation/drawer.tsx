d34d507267cc37dd00979d3b1dad0d7619613558
Araya <arayalakew@gmail.com>
2023-11-08 00:43:05 +0100

    nov 7-evening10

--- src/components/navigation/drawer.tsx
+++ src/components/navigation/drawer.tsx
@@ -14,6 +14,7 @@ import { Separator } from "../ui/separator";
 import MenuAccordion from "./menu-accordion";
 import Logo from "../data-display/logo";
 import { useState } from "react";
+import Link from "next/link";
 
 export default function Drawer() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -38,6 +39,10 @@ export default function Drawer() {
             </div>
           </SheetHeader>
           <Separator className="my-2" />
+          <Link
+            href="/cars/post"
+            className="group hidden h-10 cursor-pointer items-center justify-center gap-x-3 rounded-full px-2 duration-200 hover:bg-default-light dark:hover:bg-default-dark lg:flex"
+          >
           <MenuAccordion closeDrawer={()=>setIsOpen(false)}/>
         </SheetContent>
       </Sheet>
